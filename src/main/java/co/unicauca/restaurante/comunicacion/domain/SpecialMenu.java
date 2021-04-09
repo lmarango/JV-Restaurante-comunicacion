5cd6e7f3c1e11e5b3a3ab7c94dd250847099dbb6
Luis Arango <lmanuelarango97@outlook.es>
2021-04-08 22:28:06 -0500

    Modificado Special Menu

--- src/main/java/co/unicauca/restaurante/comunicacion/domain/SpecialMenu.java
+++ src/main/java/co/unicauca/restaurante/comunicacion/domain/SpecialMenu.java
@@ -6,18 +6,40 @@ package co.unicauca.restaurante.comunicacion.domain;
  * @author LUIS ARANGO
  */
 public class SpecialMenu {
-    private TypeDay atrDay;
+    private int smenID;
+    private String smenDay;
+    private String resID;
 
-    public SpecialMenu(TypeDay atrDay) {
-        this.atrDay = atrDay;
+    public SpecialMenu(int smenID, String smenDay, String resID) {
+        this.smenID = smenID;
+        this.smenDay = smenDay;
+        this.resID = resID;
     }
 
-    public TypeDay getAtrDay() {
-        return atrDay;
+    public SpecialMenu() {
     }
 
-    public void setAtrDay(TypeDay atrDay) {
-        this.atrDay = atrDay;
+    public int getSmenID() {
+        return smenID;
     }
-    
+
+    public void setSmenID(int smenID) {
+        this.smenID = smenID;
+    }
+
+    public String getSmenDay() {
+        return smenDay;
+    }
+
+    public void setSmenDay(String smenDay) {
+        this.smenDay = smenDay;
+    }
+
+    public String getResID() {
+        return resID;
+    }
+
+    public void setResID(String resID) {
+        this.resID = resID;
+    }
 }
